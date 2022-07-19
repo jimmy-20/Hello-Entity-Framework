be51f0ef88feb98efa3e4a821e163464545a8907
jimmy-20 <jasocanal@gmail.com>
2022-07-19 15:57:49 -0600

    Mapeo de atributos en las clases

--- Modelos/Categoria.cs
+++ Modelos/Categoria.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Proyecto.Modelos
 {
     public class Categoria
     {
+        [Key]
         public Guid IdCategoria {get;set;}
+
+        [Required]
+        [MaxLength(150)]
         public string Nombre {get;set;}
         public string Descripcion {get;set;}
         public virtual ICollection<Tarea> Tareas {get;set;}
--- Modelos/Tarea.cs
+++ Modelos/Tarea.cs
@@ -1,14 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Proyecto.Modelos
 {
     public class Tarea
     {
+        [Key]
         public Guid IdTarea {get;}
+
+        [ForeignKey("IdCategoria")]
         public Guid IdCategoria {get;set;}
+
+        [Required]
+        [MaxLength(200)]
         public string Titulo {get;set;}
         public string Descripcion {get;set;}
         public Prioridad Prioridad {get;set;}
         public DateTime FechaCreacion {get;set;}
         public virtual Categoria Categoria {get;set;}
+
+        [NotMapped]
+        public string Resumen {get;set;}
     }
 
     public enum Prioridad{
